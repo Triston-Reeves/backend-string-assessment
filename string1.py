8e2253b8e2baab9a5bf66cf3ab1563012b50a803
Triston Reeves <triston.reeves123@gmail.com>
2020-05-15 22:14:50 -0400

    first push

--- string1.py
+++ string1.py
@@ -4,7 +4,7 @@ Kenzie assignment: String1
 """
 # Your name, plus anyone who helped you with this assignment.
 # Give credit where credit is due.
-__author__ = "???"
+__author__ = "Triston Reeves"
 
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
@@ -28,7 +28,7 @@ __author__ = "???"
 
 
 def donuts(count):
-    # your code here
+    if count > 10 = many;
     return
 
 
